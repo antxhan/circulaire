e27f819737e4c559d87ba9cd175570000637bd44
Anton Hansson <125166515+antxhan@users.noreply.github.com>
2025-05-08 19:08:31 +0200

    fix: remove test section and hero animation from page

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,9 @@
-import HeroAnimation from "./_components/HeroAnimation/HeroAnimation";
-import Section from "./_components/Section";
 import HelloWorld from "./_sections/HelloWorld";
 
 export default function Home() {
   return (
     <div>
       <HelloWorld />
-      <Section title="Hello World">
-        <HeroAnimation />
-      </Section>
     </div>
   );
 }
