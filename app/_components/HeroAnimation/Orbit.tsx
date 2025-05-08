4635d90c7bf6b40e2fc25090e704ff9c85eeacb0
Anton Hansson <125166515+antxhan@users.noreply.github.com>
2025-05-08 19:41:16 +0200

    refactor: same classnames for all icon wrappers

--- app/_components/HeroAnimation/Orbit.tsx
+++ app/_components/HeroAnimation/Orbit.tsx
@@ -7,21 +7,21 @@ export default function Orbit() {
   return (
     <div className="absolute w-full h-full animate-orbit">
       <div className="relative w-full h-full">
-        <div className="animate-counter-orbit">
+        <div className="rotate-0 w-full h-full absolute">
           <Icon
-            icon={<SearchIcon width={30} height={30} />}
-            className="top-0 left-[50%] translate-[-50%]"
+            icon={<RefreshIcon width={30} height={30} />}
+            className="top-0 left-[50%] translate-[-50%] animate-counter-orbit rotate-[-0deg]"
           />
         </div>
         <div className="rotate-120 w-full h-full absolute">
           <Icon
-            icon={<RefreshIcon width={30} height={30} />}
+            icon={<DollarIcon width={30} height={30} />}
             className="top-0 left-[50%] translate-[-50%] animate-counter-orbit rotate-[-120deg]"
           />
         </div>
         <div className="rotate-240 absolute w-full h-full">
           <Icon
-            icon={<DollarIcon width={30} height={30} />}
+            icon={<SearchIcon width={30} height={30} />}
             className="top-0 left-[50%] translate-[-50%] animate-counter-orbit rotate-[-240deg]"
           />
         </div>
