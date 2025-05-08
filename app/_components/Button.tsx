010cae50114e29d0eb7baa24b24219bb8d9e6fa1
gonibarjin <179527504+gonibarjin@users.noreply.github.com>
2025-05-08 22:54:26 +0200

    feat: update ButtonLink component

--- app/_components/Button.tsx
+++ app/_components/Button.tsx
@@ -1,25 +1,23 @@
-import React from "react";
 import Link from "next/link";
 
-type ButtonLinkProps = {
+export default function ButtonLink({
+  href,
+  text = "Click me",
+  icon,
+  className,
+}: {
   href: string;
   text: string;
   icon?: React.ReactNode;
   className?: string;
-};
-
-const ButtonLink: React.FC<ButtonLinkProps> = ({
-  href,
-  text = "Click me",
-  icon,
-  className = "bg-[#1A58BB] text-white px-4 py-2 rounded-3xl font-accent font-medium text-base hover:bg-blue-600",
-}) => {
+}) {
   return (
-    <Link href={href} className={`inline-flex items-center gap-2 ${className}`}>
+    <Link
+      href={href}
+      className={`inline-flex items-center gap-2 px-4 py-2 rounded-full font-accent font-medium text-base text-white bg-blue hover:bg-blue-600 bg-neutral-000 ${className}`}
+    >
       {icon && <span>{icon}</span>}
       <span>{text}</span>
     </Link>
   );
-};
-
-export default ButtonLink;
+}
