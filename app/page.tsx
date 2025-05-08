65a417b2a52eab8b899576eb7beb7cfe8f7b8e49
Anton Hansson <125166515+antxhan@users.noreply.github.com>
2025-05-08 19:07:39 +0200

    fix: remove body border used for testing

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
+import HeroAnimation from "./_components/HeroAnimation/HeroAnimation";
+import Section from "./_components/Section";
 import HelloWorld from "./_sections/HelloWorld";
 
 export default function Home() {
   return (
     <div>
       <HelloWorld />
+      <Section title="Hello World">
+        <HeroAnimation />
+      </Section>
     </div>
   );
 }
