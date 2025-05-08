7a72b24103c62c41d386d58e260d6a23afe160f2
Anton Hansson <125166515+antxhan@users.noreply.github.com>
2025-05-08 14:02:00 +0200

    feat: import google fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Inter, League_Spartan } from "next/font/google";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -6,13 +7,22 @@ export const metadata: Metadata = {
   description: "The #1 Realtime Pricing Insights Platform",
 };
 
+const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
+const leagueSpartan = League_Spartan({
+  subsets: ["latin"],
+  variable: "--font-spartan",
+});
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html
+      lang="en"
+      className={`${inter.variable} ${leagueSpartan.variable} font-body`}
+    >
       <body className={`antialiased`}>{children}</body>
     </html>
   );
