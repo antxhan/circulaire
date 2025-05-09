d42d2eac6d5ef74508f993baad310fad2a2d7a64
Anton Hansson <125166515+antxhan@users.noreply.github.com>
2025-05-09 15:19:17 +0200

    refactor: added classname argument to section component

--- app/_components/Section.tsx
+++ app/_components/Section.tsx
@@ -4,14 +4,16 @@ import Wrapper from "./Wrapper";
 export default function Section({
   title,
   fullWidthComponent,
+  className,
   children,
 }: {
   title?: string;
   fullWidthComponent?: React.ReactNode;
+  className?: string;
   children: React.ReactNode;
 }) {
   return (
-    <section className="py-16">
+    <section className={`py-16 ${className}`}>
       <Wrapper fullWidthComponent={fullWidthComponent}>
         {title && <H2 text={title} />}
         {children}
