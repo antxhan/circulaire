4cd634533edb107df86426599b11479e10603229
gonibarjin <179527504+gonibarjin@users.noreply.github.com>
2025-05-09 14:15:36 +0200

    feat: Add target attribute

--- app/_components/ButtonLink.tsx
+++ app/_components/ButtonLink.tsx
@@ -5,15 +5,18 @@ export default function ButtonLink({
   text = "Click me",
   icon,
   className,
+  target,
 }: {
   href: string;
   text: string;
   icon?: React.ReactNode;
   className?: string;
+  target?: string;
 }) {
   return (
     <Link
       href={href}
+      target={target}
       className={`inline-flex items-center gap-2 px-4 py-2 rounded-full font-accent font-bold text-base text-neutral-000 bg-blue hover:bg-blue-600 ${className}`}
     >
       {icon && <span>{icon}</span>}
