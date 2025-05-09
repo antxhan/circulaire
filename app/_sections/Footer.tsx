7ef1ffdc6ea0de3f3c817e1c7b8b0da261d10d4f
Xutik <kseniia.prakhova@hyperisland.se>
2025-05-09 20:08:35 +0200

    fix bugs

--- app/_sections/Footer.tsx
+++ app/_sections/Footer.tsx
@@ -1,62 +1,55 @@
 import Link from "next/link";
-import Section from "../_components/Section";
+import Wrapper from "../_components/Wrapper";
 import Image from 'next/image';
 
 export default function Footer() {
   return (
-    <Section>
-      <footer className="flex justify-center h-[20rem] py-8">
-        <div className="w-1/2 flex flex-col justify-between">
-            <div className="h-[8rem] w-[8rem] bg-gray-500">animation</div>
-            <div className="flex gap-4 flex-col">
-                <div className="flex gap-2">
-                    <Link href="https://www.linkedin.com/"> 
+      <footer>
+        <Wrapper fullWidthComponent className="flex justify-center" >
+            <div className="w-1/2 flex flex-col justify-between gap-8">
+                <div className="h-[8rem] w-[8rem] bg-gray-500">animation</div>
+                <div className="flex gap-2 flex-col">
+                    <Link 
+                        href="https://www.linkedin.com/company/circulaire/"
+                        target="_blank" 
+                        rel="noopener noreferrer"
+                    > 
                         <Image
-                            // className="bg-black text-white"
-                            src="linkedin.svg"
+                            src="socials/linkedin.svg"
                             alt="LinkedIn"
                             width={20}
                             height={20}
                             // style={{ filter: 'invert(1)' }} // add it when use dark mode
                         />
                     </Link>
-                    <Link href="https://www.instagram.com/"> 
-                        <Image
-                            src="instagram.svg"
-                            alt="Instagram"
-                            width={20}
-                            height={20}
-                            // style={{ filter: 'invert(1)' }} // add it when use dark mode
-                        />
-                    </Link>
+                    <span className="text-xs">{`© ${new Date().getFullYear()} Circulaire. All rights reserved.`}</span>
                 </div>
-                <span className="text-xs">© 2025 Circulaire. All rights reserved.</span>
             </div>
-        </div>
-        <div className="w-1/2 flex flex-col justify-between">
-            <div className="h-1/2 flex justify-end">
-                <div className="w-1/3 flex flex-col ">
-                    <h4 className="font-bold font-accent">Product</h4>
-                    <nav className="flex flex-col gap-1">
-                        <Link href="/" className="text-xs"> Features</Link>
-                        <Link href="/" className="text-xs"> API</Link>
-                    </nav>
+            <div className="w-1/2 flex flex-col justify-between gap-8">
+                <div className="h-1/2 flex justify-end gap-40">
+                    <div className="flex flex-col ">
+                        <h4 className="font-bold font-accent">Product</h4>
+                        <nav className="flex flex-col gap-1">
+                            <Link href="/" className="text-xs"> Features</Link>
+                            <Link href="/" className="text-xs"> API</Link>
+                        </nav>
 
+                    </div>
+                    <div className="flex flex-col " >
+                        <h4 className="font-bold font-accent">Company</h4>
+                        <nav className="flex flex-col gap-1">
+                            <Link href="/"className="text-xs"> About</Link>
+                            <Link href="/"className="text-xs"> Press</Link>
+                        </nav>
+                    </div>
                 </div>
-                <div className="w-1/3 flex flex-col " >
-                    <h4 className="font-bold font-accent">Company</h4>
-                    <nav className="flex flex-col gap-1">
-                        <Link href="/"className="text-xs"> About</Link>
-                        <Link href="/"className="text-xs"> Press</Link>
-                    </nav>
+                <div className="h-1/2 flex justify-end align-bottom">
+                    <div className="w-1/2 bg-pink-400">
+                        Toggle
+                    </div> 
                 </div>
             </div>
-            <div className="h-1/2 flex justify-end align-bottom">
-                {/* Toggle here */}
-                <div className="w-1/2 bg-pink-400"></div> 
-            </div>
-        </div>
+        </Wrapper>
       </footer>
-    </Section>
   );
 }
