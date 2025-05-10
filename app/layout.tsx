104efb67c25a44818fa7f683b2d195ac93e4dcfc
Raana <raana.barimani@hyperisland.se>
2025-05-10 18:45:17 +0200

    deleted import Nav

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
-import Head from 'next/head';
+import Head from "next/head";
 import { Inter, League_Spartan } from "next/font/google";
 import "./globals.css";
-import Nav from "./_components/Navbar/Nav";
+
 import Header from "./_components/Header/Header";
 
 export const metadata: Metadata = {
@@ -16,26 +16,26 @@ const leagueSpartan = League_Spartan({
   variable: "--font-spartan",
 });
 
-
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-
     <html
       lang="en"
       className={`${inter.variable} ${leagueSpartan.variable} font-body`}
     >
       <Head>
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=League+Spartan:wght@400;500;700&display=swap" />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=League+Spartan:wght@400;500;700&display=swap"
+        />
       </Head>
       <body className={`antialiased`}>
-        <Header/>
+        <Header />
         {children}
       </body>
-      
     </html>
   );
 }
