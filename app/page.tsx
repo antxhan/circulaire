15b0a490d8b6995a8c7d41d5654bfb0e319b7264
Anton Hansson <125166515+antxhan@users.noreply.github.com>
2025-05-12 18:50:51 +0200

    refactor: header colors and responsiveness + sticky and blurry bg

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,11 @@ import HelloWorld from "./_sections/HelloWorld";
 export default function Home() {
   return (
     <div>
-      <HelloWorld />    
+      <HelloWorld />
+      <HelloWorld />
+      <HelloWorld />
+      <HelloWorld />
+      <HelloWorld />
     </div>
   );
 }
