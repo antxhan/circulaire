9cd42c6558b1231efa068d4cee69082ccbeb3762
Xutik <kseniia.prakhova@hyperisland.se>
2025-05-12 16:19:46 +0200

    pages.tsx edited

--- app/_sections/TailorMadeFor.tsx
+++ app/_sections/TailorMadeFor.tsx
@@ -4,7 +4,7 @@ import { TextRotator } from "../_components/TextRotator";
  const customerCategories = ['Second Hand Shops', 'Professional Traders', 'Marketplaces', ' Insurance Companies', 'Pawn Shops', 'Personal Shoppers', 'Auction Houses'];
  
  export const TailorMadeFor = () => (
-     <Section className="flex items-center gap-4">
+     <Section>
          <h2 className="text-2xl font-bold text-black whitespace-nowrap leading-[80px] height-[80px]">
              Tailor-made for
          </h2>
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
 import HelloWorld from "./_sections/HelloWorld";
-import { TailorMadeFor } from "./_sections/TailorMadeFor";
 
 export default function Home() {
   return (
     <div>
       <HelloWorld />
-      <TailorMadeFor />
     </div>
   );
 }
