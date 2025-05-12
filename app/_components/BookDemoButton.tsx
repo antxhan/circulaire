d66185e519f93560acb11cbd1c3220cb2d68e3f3
Anton Hansson <125166515+antxhan@users.noreply.github.com>
2025-05-12 18:33:30 +0200

    refactor: Book button use imported synced href

--- app/_components/BookDemoButton.tsx
+++ app/_components/BookDemoButton.tsx
@@ -1,12 +1,13 @@
 import ButtonLink from "./ButtonLink";
 import CalendarIcon from "../_icons/CalendarIcon";
+import { links } from "../_constants/links";
 
 export default function BookDemoButton() {
-    return (
-        <ButtonLink
-        href="https://calendar.app.google/wtUEmDXJM6m6hm4bA"
-        text="Book a Demo"
-        icon={<CalendarIcon />}
-        />
-    )
+  return (
+    <ButtonLink
+      href={links.booking}
+      text="Book a Demo"
+      icon={<CalendarIcon />}
+    />
+  );
 }
