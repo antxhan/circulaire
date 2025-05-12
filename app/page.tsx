3d62556042d8273d649d9e070d1f9cb26894318a
gonibarjin <179527504+gonibarjin@users.noreply.github.com>
2025-05-12 12:39:39 +0200

    Revert page.tsx to original

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
 import HelloWorld from "./_sections/HelloWorld";
-import AnimateScrollHorizontal from "./_components/LogoBannerScroll";
 
 export default function Home() {
   return (
     <div>
       <HelloWorld />
-      <AnimateScrollHorizontal />
     </div>
   );
 }
