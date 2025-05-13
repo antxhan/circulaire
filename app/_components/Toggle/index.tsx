5bdb22b4a0525c9f87a7c8b78a690f8c111a6c6e
Anton Hansson <125166515+antxhan@users.noreply.github.com>
2025-05-14 00:08:26 +0200

    style: toggle colors and size

--- app/_components/Toggle/index.tsx
+++ app/_components/Toggle/index.tsx
@@ -1,42 +1,44 @@
 type ToggleItemProps = {
-    id: string;
-    content: React.ReactNode;
+  id: string;
+  content: React.ReactNode;
 };
 
 type ToggleProps = {
-    items: ToggleItemProps[];
-    activeId: string;
-    onClick: (id: string) => void;
-    className?: string;
+  items: ToggleItemProps[];
+  activeId: string;
+  onClick: (id: string) => void;
+  className?: string;
 };
 
 const getItemClasses = (isActive: boolean) => `
-flex items-center justify-center px-6 h-full rounded-full transition-all duration-200 ease-in-out cursor-pointer
+flex items-center justify-center px-6 rounded-full !h-11 transition-all duration-200 ease-in-out cursor-pointer
 ${
-    isActive
-        ? 'bg-blue-500 text-white dark:bg-blue-600'
-        : 'hover:bg-opacity-10 hover:bg-gray-500'
+  isActive
+    ? "bg-blue text-white"
+    : "hover:bg-opacity-10 hover:text-blue-500 dark:hover:text-white"
 }
 `;
 
 export const Toggle = ({
-    items,
-    activeId,
-    onClick,
-    className = ''
+  items,
+  activeId,
+  onClick,
+  className = "",
 }: ToggleProps) => {
-    return (
-        <div className={`flex items-center h-[72px] rounded-full p-1 w-fit bg-gray-100 text-gray-800 border border-gray-100 dark:bg-gray-800 dark:text-white dark:border-gray-100 ${className}`}>
-            {items.map(item => (
-                <button
-                    key={item.id}
-                    type="button"
-                    className={getItemClasses(activeId === item.id)}
-                    onClick={() => onClick(item.id)}
-                >
-                    {item.content}
-                </button>
-            ))}
-        </div>
-    );
+  return (
+    <div
+      className={`flex p-2 items-center rounded-full font-accent font-bold w-fit bg-neutral-200 text-gray-800 dark:border dark:bg-neutral-000 dark:text-neutral-600 dark:border-neutral-700 ${className}`}
+    >
+      {items.map((item) => (
+        <button
+          key={item.id}
+          type="button"
+          className={getItemClasses(activeId === item.id)}
+          onClick={() => onClick(item.id)}
+        >
+          {item.content}
+        </button>
+      ))}
+    </div>
+  );
 };
