ee73c1f13424033a373c7a8edf83fbe067756c08
Anton Hansson <125166515+antxhan@users.noreply.github.com>
2025-05-14 00:25:02 +0200

    fix: add scroll margin top for when navigating to sections

--- app/_sections/Header.tsx
+++ app/_sections/Header.tsx
@@ -6,7 +6,7 @@ import { links } from "../_constants/links";
 
 export default function Header() {
   return (
-    <header className="bg-neutral-100/80 backdrop-blur-md sticky top-0 z-9998">
+    <header className="bg-neutral-100/80 backdrop-blur-md sticky w-full top-0 z-9998">
       <Wrapper className="flex items-center gap-12 font-accent h-24 font-bold">
         <Link href={links.home} className="hover:!text-neutral-900">
           <WordMarkLogo />
