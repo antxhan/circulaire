d553bba746ff5882aac60d8a76b536632296fab1
gonibarjin <179527504+gonibarjin@users.noreply.github.com>
2025-05-13 19:04:13 +0200

    Updated Hero with h1

--- app/_sections/Hero.tsx
+++ app/_sections/Hero.tsx
@@ -3,7 +3,6 @@ import ButtonLink from "../_components/ButtonLink";
 import BlogUpdate from "../_components/BlogUpdate";
 import HeroAnimation from "../_components/HeroAnimation";
 import Section from "../_components/Section";
-import H2 from "../_components/H2";
 import { links } from "../_constants/links";
 import VideoIcon from "../_icons/VideoIcon";
 
@@ -11,12 +10,12 @@ export default function HeroSection() {
   return (
     <Section>
       <div className="flex flex-col md:flex-row">
-        <div className="flex-1 ml-6 sm:ml-12 lg:ml-20">
+        <div className="flex-1 ml-6 sm:ml-12 lg:ml-25">
           <BlogUpdate text="Circulair March Update" link="#" />
-          <H2 className="mt-12 leading-tight">
+          <h1 className="font-accent font-bold text-5xl mt-12 leading-tight">
             The #1 Real-Time
             <br /> Pricing Insights Platform
-          </H2>
+          </h1>
           <ul className="mt-4 space-y-2 font-body list-disc list-outside pl-4 max-w-lg">
             <li>
               AI-powered accurate, modern and powerful platform for second-hand
