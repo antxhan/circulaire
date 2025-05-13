7565c927a7d834ef824f779d9c3a56fbe3821517
Raana <raana.barimani@hyperisland.se>
2025-05-13 14:06:20 +0200

    remove max-w-xl, the section will handle the max-width.

--- app/_components/Video.tsx
+++ app/_components/Video.tsx
@@ -2,7 +2,7 @@ export default function VideoPlayer() {
   return (
     <video
       controls
-      className="w-full max-w-xl h-auto rounded-xl border border-neutral-300"
+      className="w-full h-auto rounded-xl border border-neutral-300"
     >
       <source src="/video.mp4" type="video/mp4" />
       Your browser does not support the video tag.
