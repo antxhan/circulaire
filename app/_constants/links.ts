f3c5fb3530d3948edc3f2bf83e89f8c3ae6c030b
Anton Hansson <125166515+antxhan@users.noreply.github.com>
2025-05-13 17:13:42 +0200

    refactor: add home

--- app/_constants/links.ts
+++ app/_constants/links.ts
@@ -5,6 +5,7 @@ export const links = {
   api: signInUrl,
   booking: "https://calendar.app.google/wtUEmDXJM6m6hm4bA",
   features: "#features",
+  home: "/",
   signIn: signInUrl,
   press:
     "https://journal.gocirculaire.com/circulaire-revolutionerar-prissattning-av-second-hand-produkter-med-ai-tar-in-kapital-och-starker-styrelsen/",
