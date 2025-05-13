712a7c10ecd5e3e5e7a702ba5d6dd938241cf875
gonibarjin <179527504+gonibarjin@users.noreply.github.com>
2025-05-13 11:22:57 +0200

    small tweaks testimonial cards

--- app/_components/TestimonialCard.tsx
+++ app/_components/TestimonialCard.tsx
@@ -15,30 +15,21 @@ export default function TestimonialCard({
   return (
     <Card className="p-4 flex flex-col gap-2 max-w-sm border-none">
       <div className="flex items-center gap-2">
-        <div className="w-[38px] h-[38px] rounded-full overflow-hidden">
+        <div className="w-9 h-9 rounded-full overflow-hidden">
           <Image
             src={imageSrc}
             alt={`${name}'s profile picture`}
-            width={38}
-            height={38}
+            width={36}
+            height={36}
             className="object-cover w-full h-full"
           />
         </div>
         <div className="leading-[1]">
-          <p
-            className="text-[20px] font-semibold text-white mb-0"
-            style={{ fontFamily: "var(--font-accent)" }}
-          >
-            {name}
-          </p>
-          <p className="text-[14px] font-extralight text-white mt-0">
-            {profession}
-          </p>
+          <h6 className="text-xl font-bold font-accent">{name}</h6>
+          <span className="text-sm font-normal">{profession}</span>
         </div>
       </div>
-      <p className="text-[20px] italic font-light text-white leading-[1.1]">
-        &quot;{comment}&quot;
-      </p>
+      <p className="text-xl italic leading-[1.1]">&quot;{comment}&quot;</p>
     </Card>
   );
 }
--- app/_sections/WhatUsersSay.tsx
+++ app/_sections/WhatUsersSay.tsx
@@ -28,7 +28,7 @@ const testimonials = [
 export default function WhatUsersSay() {
   return (
     <Section title="What our users are saying">
-      <div className="mt-12 flex flex-col md:flex-row justify-center gap-4 max-w-5xl mx-auto">
+      <div className="mt-12 flex flex-col md:flex-row justify-center gap-4 mx-auto">
         {testimonials.map((testimonials, index) => (
           <TestimonialCard key={index} {...testimonials} />
         ))}
