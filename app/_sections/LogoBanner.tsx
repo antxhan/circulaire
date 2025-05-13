6ce41fe4a03485f36f43a2de0038a25379be57b1
gonibarjin <179527504+gonibarjin@users.noreply.github.com>
2025-05-13 11:55:47 +0200

    feat: Added logo banner section

---
+++ app/_sections/LogoBanner.tsx
@@ -0,0 +1,12 @@
+import Section from "../_components/Section";
+import H2 from "../_components/H2";
+import LogoBannerScroll from "../_components/LogoBannerScroll";
+
+export default function LogoBannerSection() {
+  return (
+    <Section title="Placeholder Title">
+      <H2 className="mb-12 ml-6">PlaceHolder Title</H2>
+      <LogoBannerScroll />
+    </Section>
+  );
+}
