dba7f07af1d2159d340f4180dff6a43b6236a25b
hannac7 <yourname@example.com>
2025-05-13 21:46:27 +0200

    Created HowItWorks section

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import Features from "./_sections/Features";
 import HelloWorld from "./_sections/HelloWorld";
 import { TailorMadeFor } from "./_sections/TailorMadeFor";
+import HowItWorks from "./_sections/HowItWorks";
 
 export default function Home() {
   return (
     <div>
       <HelloWorld />
       <Features />
+      <HowItWorks />
       <TailorMadeFor />
     </div>
   );
