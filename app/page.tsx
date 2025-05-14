62c38ed0600a1f5831d873f36125c370cee5ff8f
hannac7 <yourname@example.com>
2025-05-14 11:32:58 +0200

    edited merge conflicts

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,9 @@
 import Features from "./_sections/Features";
-import HelloWorld from "./_sections/HelloWorld";
-<<<<<<< HEAD
-import { TailorMadeFor } from "./_sections/TailorMadeFor";
-<<<<<<< HEAD
-import WeAreCirculaire from "./_sections/WeAreCirculaire";
-=======
+import HelloWorld from "./_sections/HelloWorld";;
 import Testimonials from "./_sections/Testimonials";
 import HowItWorks from "./_sections/HowItWorks";
->>>>>>> main
-=======
-import HowItWorks from "./_sections/HowItWorks";
 import { TailorMadeFor } from "./_sections/TailorMadeFor";
 import WeAreCirculaire from "./_sections/WeAreCirculaire";
->>>>>>> fad29c9f8112eac8521dae5f6408416cba311ef8
 
 export default function Home() {
   return (
@@ -21,11 +12,8 @@ export default function Home() {
       <Features />
       <HowItWorks />
       <TailorMadeFor />
-<<<<<<< HEAD
       <WeAreCirculaire />
-=======
       <Testimonials />
->>>>>>> main
     </div>
   );
 }
