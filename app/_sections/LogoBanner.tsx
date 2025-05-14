343f46f4373ccd1c97b4b18e523021248ca933b6
Anton Hansson <125166515+antxhan@users.noreply.github.com>
2025-05-14 21:23:25 +0200

    fix: logo banner fulll width + no background

--- app/_sections/LogoBanner.tsx
+++ app/_sections/LogoBanner.tsx
@@ -4,11 +4,10 @@ import LogoBannerScroll from "../_components/LogoBannerScroll";
 
 export default function LogoBannerSection() {
   return (
-    <Section>
+    <Section fullWidthComponent={<LogoBannerScroll />}>
       <H2 className="mb-12 md:ml-6 text-center md:text-left text-3xl md:text-5xl">
-        PlaceHolder Title
+        Integrate with
       </H2>
-      <LogoBannerScroll />
     </Section>
   );
 }
