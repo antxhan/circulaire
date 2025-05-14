34ae8e3bc32b08f5ed54dc11591db0ba8538ea6c
Anton Hansson <125166515+antxhan@users.noreply.github.com>
2025-05-14 17:31:47 +0200

    refactor: add Hero to page and delete HelloWorld

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,14 @@
 import Features from "./_sections/Features";
-import HelloWorld from "./_sections/HelloWorld";
 import Testimonials from "./_sections/Testimonials";
 import Demo from "./_sections/Demo";
 import { TailorMadeFor } from "./_sections/TailorMadeFor";
 import WeAreCirculaire from "./_sections/WeAreCirculaire";
+import Hero from "./_sections/Hero";
 
 export default function Home() {
   return (
     <div>
-      <HelloWorld />
+      <Hero />
       <Features />
       <Demo />
       <TailorMadeFor />
