aab9f1bd95d298f5f7f7db12dd655bdb7f4961d7
Anton Hansson <125166515+antxhan@users.noreply.github.com>
2025-05-14 23:19:06 +0200

    style: remove random margin-left on integration section h2

--- app/_sections/Integrations/index.tsx
+++ app/_sections/Integrations/index.tsx
@@ -5,7 +5,7 @@ import LogoBannerScroll from "./LogoBannerScroll";
 export default function LogoBannerSection() {
   return (
     <Section fullWidthComponent={<LogoBannerScroll />}>
-      <H2 className="mb-12 md:ml-6 text-center md:text-left text-3xl md:text-5xl">
+      <H2 className="mb-12 text-center md:text-left text-3xl md:text-5xl">
         Integrate with
       </H2>
     </Section>
