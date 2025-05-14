a6f7ea430ebc9e4a4eb80a642309b5a5725e27d4
Anton Hansson <125166515+antxhan@users.noreply.github.com>
2025-05-14 23:45:52 +0200

    refactor: only one word in video toggle + hide book demo button at lg + center toggle and h2 on mobile

--- app/_components/Toggle/index.tsx
+++ app/_components/Toggle/index.tsx
@@ -4,8 +4,8 @@ type ToggleItemProps = {
 };
 
 const getItemClasses = (isActive: boolean) => `
-flex items-center justify-center flex-1 text-center px-4 py-2 rounded-full !h-11 transition-all duration-200 ease-in-out cursor-pointer
-  whitespace-normal break-words leading-tight
+flex items-center justify-center flex-1 text-center px-4 gap-2 py-2 rounded-full !h-11 transition-all duration-200 ease-in-out cursor-pointer
+  text-nowrap break-words leading-tight
 ${
   isActive
     ? "bg-blue text-white"
